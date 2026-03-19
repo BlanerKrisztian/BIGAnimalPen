d3de5c4f0808cbf9230a27dbe4e790cecb77902f
Blaner Krisztián <barkiza760bc@gmail.com>
2026-03-19 14:29:23 +0100

    uploaded projekt

---
+++ Slopjekt/Slopjekt/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Slopjekt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
